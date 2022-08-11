dca510420b1960089da46df0d24345dc2d897211
Timi Ombe <timmyombe@gmail.com>
2022-08-11 13:11:05 +0100

    Finished 21-at-most.js

--- problems/21-at-most.js
+++ problems/21-at-most.js
@@ -22,13 +22,32 @@ console.log(atMost(['boat', 'arc', 'cat', 'car', 'academy'], 1, startsWithA));  
 
 *******************************************************************************/
 
-let atMost = function() {
-
+let atMost = function(array, maxNum, cb) {
+    let totalTruths = 0;
+    array.forEach(function (elem, i) {
+        if (cb(elem, i, array)) {
+            totalTruths += 1;
+        }
+    });
+    return (totalTruths <= maxNum);
 };
 
 
+// Tests
 
+let isPositive = function (n) {
+    return n > 0;
+};
+let startsWithA = function (s) {
+    return s[0].toUpperCase() === 'A';
+};
 
+console.log(atMost([6, -2, 4, -1], 3, isPositive));                             // true
+console.log(atMost([6, -2, 4, 1], 3, isPositive));                              // true
+console.log(atMost([6, 2, 4, 1], 3, isPositive));                               // false
+console.log(atMost(['boat', 'cat', 'car'], 1, startsWithA));                    // true
+console.log(atMost(['boat', 'cat', 'car', 'academy'], 1, startsWithA));         // true
+console.log(atMost(['boat', 'arc', 'cat', 'car', 'academy'], 1, startsWithA));  // false
 
 
 /*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
