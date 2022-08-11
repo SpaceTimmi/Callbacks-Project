aa180701b22d2cd98ec9fd3674b766e1da6341a2
Timi Ombe <timmyombe@gmail.com>
2022-08-11 10:57:38 +0100

    05-selective-map.js

--- problems/05-selective-map.js
+++ problems/05-selective-map.js
@@ -42,13 +42,46 @@ AFTER YOU ARE FINISHED WITH THIS PROBLEM, ASK FOR A CODE REVIEW
 - How many times are you calling each callback function?
 *******************************************************************************/
 
-let selectiveMap = function() {
-
+let selectiveMap = function(array, cb1, cb2) {
+    let newArr = [];
+    array.forEach(function (elem, i) {
+        if (cb1(elem, i, array)) {
+            let result = cb2(elem, i, array);
+            newArr.push(result);
+        } else {
+            newArr.push(elem)
+        }
+    });
+    return newArr
 };
 
 
 
+// Tests
+function isEven(n) {
+    return n % 2 === 0;
+}
 
+function isPositive(n) {
+    return n > 0;
+}
+
+function square(n) {
+    return n * n;
+}
+
+function flipSign(n) {
+    return n * -1;
+}
+
+console.log(selectiveMap([8, 5, 10, 4], isEven, square));
+// [ 64, 5, 100, 16 ]
+
+console.log(selectiveMap([-10, 4, 7, 6, -2, -9], isEven, flipSign));
+// [ 10, -4, 7, -6, 2, -9 ]
+
+console.log(selectiveMap([-10, 4, 7, 6, -2, -9], isPositive, square));
+// [-10, 16, 49, 36, -2, -9]
 
 
 /*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
