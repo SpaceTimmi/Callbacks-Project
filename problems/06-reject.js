1c917f3314b83645deedecfbb120c1571340c37e
Timi Ombe <timmyombe@gmail.com>
2022-08-11 11:01:02 +0100

    Finished 06-reject.js

--- problems/06-reject.js
+++ problems/06-reject.js
@@ -21,14 +21,27 @@ let hasA = function(s) {
 console.log(reject(['breadth', 'GRAPH', 'depth', 'height'], hasA)); // [ 'depth', 'height' ]
 *******************************************************************************/
 
-let reject = function() {
-
+let reject = function(array, cb) {
+    let newArr = [];
+    array.forEach(function (elem, i) {
+        if (!cb(elem, i, array)) {
+            newArr.push(elem)
+        }
+    });
+    return newArr;
 };
 
 
+// Tests
+let isEven = function(n) {
+    return n % 2 === 0;
+};
+console.log(reject([7, 8, 5, 6, 12, 11], isEven)); // [ 7, 5, 11 ]
 
-
-
+let hasA = function(s) {
+    return s.toLowerCase().includes('a');
+};
+console.log(reject(['breadth', 'GRAPH', 'depth', 'height'], hasA)); // [ 'depth', 'height' ]
 
 /*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
 module.exports = reject;
