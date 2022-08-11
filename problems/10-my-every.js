a0f8e3db1af52710cc65ea629840bf5ff6462d71
Timi Ombe <timmyombe@gmail.com>
2022-08-11 11:18:38 +0100

    Finished 10-my-every.js

--- problems/10-my-every.js
+++ problems/10-my-every.js
@@ -21,13 +21,28 @@ console.log(myEvery(['book', 'door'], hasO));           // true
 console.log(myEvery(['book', 'door', 'pen'], hasO));    // false
 *******************************************************************************/
 
-let myEvery = function() {
-
+let myEvery = function(array, cb) {
+    let result = array.reduce(function (acc, elem, i) {
+        let cbResult = cb(elem, i, array); 
+        return (acc && cbResult)
+    }, true);
+    return result; 
 };
 
 
+// Tests
+let isEven = function (num) {
+    return num % 2 === 0;
+};
 
+let hasO = function(string) {
+    return string.includes('o');
+};
 
+console.log(myEvery([4, 8, 6, 10], isEven));            // true
+console.log(myEvery([4, 7, 6, 10], isEven));            // false
+console.log(myEvery(['book', 'door'], hasO));           // true
+console.log(myEvery(['book', 'door', 'pen'], hasO));    // false
 
 
 /*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
