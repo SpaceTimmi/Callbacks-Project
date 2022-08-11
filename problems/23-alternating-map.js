f63bdd5e67f2d7e8e0974b997888f872cb13b104
Timi Ombe <timmyombe@gmail.com>
2022-08-11 13:19:06 +0100

    Finished 23-alternating-map.js

--- problems/23-alternating-map.js
+++ problems/23-alternating-map.js
@@ -36,10 +36,38 @@ console.log(alternatingMap(['hEy', 'EVERYone', 'whats', 'uP??'], yell, whisper))
 *******************************************************************************/
 
 
-let alternatingMap = function() {
-
+let alternatingMap = function(array, cb1, cb2) {
+    let result = array.map(function (elem, i) {
+        if (i % 2 === 0) {
+            return cb1(elem);
+        }
+        return cb2(elem);
+    });
+    return result;
 };
 
+// Tests
+
+let triple = function (n) {
+    return 3 * n;
+};
+
+let half = function (n) {
+    return n / 2;
+};
+console.log(alternatingMap([7, 3, 2, 9, 8], triple, half));
+// [ 21, 1.5, 6, 4.5, 24 ]
+
+
+let yell = function (s) {
+    return s.toUpperCase() + '!';
+};
+
+let whisper = function (s) {
+    return '..' + s.toLowerCase() + '..';
+};
+console.log(alternatingMap(['hEy', 'EVERYone', 'whats', 'uP??'], yell, whisper));
+// [ 'HEY!', '..everyone..', 'WHATS!', '..up??..' ]
 
 
 
